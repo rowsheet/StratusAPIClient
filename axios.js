f401192949aa5d71f0719e02610c27899dbd74e4
rowsheet <rowsheet.com@gmail.com>
2021-02-08 07:14:42 -0700

    Working.

--- axios.js
+++ axios.js
@@ -1,25 +1,60 @@
 const axios = require('axios');
 const { DOMParser } = require('xmldom')
 
-// var url = 'https://stratus-api-heroku.herokuapp.com/api/core/auth?wsdl';
-var url = 'http://localhost:9000/api/core/auth?wsdl';
+// var BASE_URL = 'http://localhost:9000/api/';
+var BASE_URL = 'https://stratus-api-heroku.herokuapp.com/api/';
 
-let xmls='<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"\
-                            xmlns:web="http://coreAuth.com/">\
-            <soapenv:Header/>\
-            <soapenv:Body>\
-              <web:signin>\
-                <web:username>INR</web:username>\
-                <web:password>USD</web:password>\
-              </web:signin>\
-            </soapenv:Body>\
-          </soapenv:Envelope>';
+function build_tag(label, value) {
+	return '<web:' + label + '>' + value + '</web:' + label + '>'
+}
 
-axios.post(url,xmls,
-           {headers:
-             {'Content-Type': 'text/xml'}
-           }).then(res=>{
-		   var doc = new DOMParser().parseFromString(res.data);
-		   var json = doc.getElementsByTagName("return")[0].childNodes[0].data;
-		   console.log(json);
-           }).catch(err=>{console.log(err)});
+args = {
+	"username": "akleinhans",
+	"password": "myverylongpassword",
+}
+
+function build_args(args) {
+	sum = '';
+	for (var key of Object.keys(args)) {
+		sum += build_tag(key, args[key]);
+	}
+	return sum;
+}
+
+function build_xml(namespace, method, args) {
+	var string ='<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:web="http://' + namespace + '.com/"><soapenv:Header/><soapenv:Body>' + wrap_method(method, args) + '</soapenv:Body></soapenv:Envelope>';
+	return string;
+}
+
+function wrap_method(method, args) {
+	return '<web:' + method + '>' + build_args(args) + '</web:' + method + '>';
+}
+
+function call_api(url, namespace, method, args) {
+	var xmls = build_xml(namespace, method, args);
+	axios.post(url,xmls,
+		   {headers:
+		     {'Content-Type': 'text/xml'}
+		   }).then(res=>{
+			   var doc = new DOMParser().parseFromString(res.data);
+			   var json = doc.getElementsByTagName("return")[0].childNodes[0].data;
+			   console.log(json);
+		   }).catch(err=>{
+	//		   console.error("Got error.");
+			   console.log(err)
+		   });
+}
+
+function endpoint(path) {
+	return BASE_URL + path + "?wsdl";
+}
+
+url = endpoint('core/auth');
+namespace = 'coreAuth';
+method = 'signin';
+
+// console.log(build_tag('username', 'USERNAME'));
+// console.log(build_args(args));
+// console.log(wrap_method(method, args));
+// console.log(build_xml(namespace, method, args));
+console.log(call_api(url, namespace, method, args));
